cc12d0d0f58b7ec3eaf1f70aea642255afa53995
TatsiaA <taniokk@mail.ru>
2021-09-26 19:15:05 +0300

    feat: add improvements for sections

--- museum/range.js
+++ museum/range.js
@@ -1,11 +1,11 @@
 const progress = document.querySelector('.progress__range');
 progress.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #ffffff ${value}%, #ffffff 100%)`
 })
 
 const progress_volume = document.querySelector('.progress__range__volume');
 progress_volume.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #ffffff ${value}%, #ffffff 100%)`
 })
