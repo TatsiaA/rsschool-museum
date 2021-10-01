44ac43e019c6a5ce524590d5fb117f4cc7c48318
TatsiaA <taniokk@mail.ru>
2021-10-02 01:55:57 +0300

    feat: add styles

--- museum/range.js
+++ museum/range.js
@@ -11,32 +11,26 @@ progress_volume.addEventListener('input', function() {
 })
 
 function randomize() {
-  
       for (var i = rando.children.length; i >= 0; i--){ rando.appendChild(rando.children[Math.random() * i | 0]);
   
       }
   
   }
   
-  function makeVis(j) {
-  
-      var photo = rando.children[j].firstElementChild;
-  
+  function makeVis(j) {  
+      var photo = rando.children[j].firstElementChild;  
       setTimeout(function() { photo.classList.add("visible"); }, 700 * j);
 
   }
   
   function sequentize(){
-      for (var j = 0; j <= rando.children.length; ++j)
-  
+      for (var j = 0; j <= rando.children.length; ++j)  
           makeVis(j);
   
   }
   
   function fsort() {
-  
-      randomize();
-  
+      randomize();  
       sequentize();
   
   }
