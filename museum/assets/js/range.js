b4ac680d06a2b8f98da61afc766411ef57bdd893
TatsiaA <taniokk@mail.ru>
2021-10-05 01:52:47 +0300

    feat: add styles for 1024px

--- museum/assets/js/range.js
+++ museum/assets/js/range.js
@@ -10,33 +10,4 @@ progress_volume.addEventListener('input', function() {
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #ffffff ${value}%, #ffffff 100%)`
 })
 
-function randomize() {
-      for (var i = rando.children.length; i >= 0; i--){ rando.appendChild(rando.children[Math.random() * i | 0]);
-  
-      }
-  
-  }
-  
-  function makeVis(j) {  
-      var photo = rando.children[j].firstElementChild;  
-      setTimeout(function() { photo.classList.add("visible"); }, 700 * j);
-
-  }
-  
-  function sequentize(){
-      for (var j = 0; j <= rando.children.length; ++j)  
-          makeVis(j);
-  
-  }
-  
-  function fsort() {
-      randomize();  
-      sequentize();
-  
-  }
-
-  var rando = document.getElementById('galler');
-
-fsort();
-
   
